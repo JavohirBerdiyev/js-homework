3c4f0ccbb6650bf940259d9a426450b9d161bdab
j.berdiyev <j.berdiyev@uic.group>
2023-11-27 16:11:05 +0500

    initial commit

---
+++ if/if15.js
@@ -0,0 +1,13 @@
+let n = 34300;
+
+if(n >= 10000) {
+  console.log(5); 
+} else if(n >= 1000) { 
+  console.log(4);
+} else if(n >= 100) { 
+  console.log(3);
+} else if(n >= 10) { 
+  console.log(2);
+}else if(n >= 0) { 
+  console.log(1);
+}
